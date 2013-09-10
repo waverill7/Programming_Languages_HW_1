ca4a5b2a8aa94ae2f2f8f1ccec6b64508b4faeb0
waverill7 <waverill@lion.lmu.edu>
2013-09-09 20:33:04 -0700

    Update interleave.py

--- Functions/interleave.py
+++ Functions/interleave.py
@@ -2,8 +2,8 @@ def interleave( a1, a2 ):
     result = []
     if len( a1 ) == len( a2 ):
         for x in range( 0, len( a1 ) ):
-        result.append( a1[ x ] )
-        result.append( a2[ x ] )
+            result.append( a1[ x ] )
+            result.append( a2[ x ] )
     elif len( a1 ) > len( a2 ):
         for x in range( 0, len( a2 ) ):
             result.append( a1[ x ] )
