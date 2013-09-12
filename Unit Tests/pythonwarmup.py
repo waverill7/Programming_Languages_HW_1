a654b458a4b899cc959f451f80df66c0dbbdc27d
waverill7 <waverill@lion.lmu.edu>
2013-09-11 23:30:12 -0700

    Update pythonwarmup.py

--- Unit Tests/pythonwarmup.py
+++ Unit Tests/pythonwarmup.py
@@ -4,9 +4,7 @@ import random;
 def change( cents ):
     coins = [ 25, 10, 5, 1 ]
     for x in range( 0, 4 ):
-        y = divmod( cents, coins[ x ] )
-        coins[ x ] = y[ 0 ]
-        cents = y[ 1 ]
+        coins[ x ], cents = divmod( cents, coins[ x ] )
     return tuple( coins )
 
 #Problem 2:
