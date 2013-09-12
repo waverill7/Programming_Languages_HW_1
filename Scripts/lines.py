defa72291f18346cbd5e4b91d988c1c91ec18ca0
waverill7 <waverill@lion.lmu.edu>
2013-09-11 22:54:28 -0700

    Update lines.py

--- Scripts/lines.py
+++ Scripts/lines.py
@@ -3,20 +3,17 @@ import sys;
 fileName = sys.argv[ 1 ]
 fileData = open( fileName, "r" )
 
-count = 0
+count = 0;
 whiteSpace = [ "\n", " ", "\t" ]
-aBlankLine = True
 
 for line in fileData:
     if line[ 0 ] != "#":
         for c in line:
             if c not in whiteSpace:
-                aBlankLine = False
+                count += 1
                 break
-        if not aBlankLine:
-            count += 1
-            aBlankLine = True
 
 print count
 
-fileData.close()       
+fileData.close()
+    
